18deffabdc7c1e8e80314ebec21bcc77d05cdb27
Ekaterina Verbitskaia <kajigor@gmail.com>
2020-08-06 00:31:17 +0300

    Examples from the APLAS evaluation

---
+++ examples/doubleApp.pl
@@ -0,0 +1,5 @@
+doubleAppendo(X, Y, Z, R) <- appendo(X, Y, T), appendo(T, Z, R).
+appendo(nil, Y, Y).
+appendo(cons(H, T), Y, cons(H, Ty)) <- appendo(T, Y, Ty).
+
+<- doubleAppendo(X__,Y,Z,R).
---
+++ examples/move.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+dir=/home/ev/mk/papers/aplas/benchmarks
+
+cp geoff_doubleApp.pl $dir/doubleAppendo/geoff.pl
+cp geoff_maxLengtho.pl $dir/maxLengtho/geoff.pl
+cp geoff_path.pl $dir/path/geoff.pl
+cp geoff_unify.pl $dir/unify/geoff.pl
+cp geoff_original*.pl $dir/propEval/
+#cp geoff_doubleApp.pl $dir/lTypechecker/geoff.pl
+
+#cp geoff_doubleApp.ml $dir/doubleAppendo/geoff.ml
+#cp geoff_maxLengtho.ml $dir/maxLengtho/geoff.ml
+#cp geoff_path.ml $dir/path/geoff.ml
+#cp geoff_unify.ml $dir/unify/geoff.ml
+#cp geoff_original*.ml $dir/propEval/
+#cp geoff_doubleApp.ml $dir/lTypechecker/geoff.ml
+
